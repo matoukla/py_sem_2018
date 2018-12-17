81f9453d09240efddc982013199d60bd5db90384
matoukla <klar.mat@seznam.cz>
2018-12-17 15:29:34 +0100

    Added simple test file

---
+++ test.py
@@ -0,0 +1,11 @@
+# Simple filter tests
+import numpy as np
+import filters as f
+import load as l
+#----------------------------------------------------------------------
+path = "C:/Users/klarm/Documents/pyth/py_sem_2018/kvetina.jpg"
+l.make_dir ( path )
+data_0 = l.data_image( path )
+
+f.inv ( data_0, path )
+f.gs ( data_0, path )
