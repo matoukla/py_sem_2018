ebc12528efb55b3a53d83db94a8365f13e796e1e
matoukla <klar.mat@seznam.cz>
2018-12-17 22:27:21 +0100

    Added blur functions

--- filters.py
+++ filters.py
@@ -75,6 +75,39 @@ def bgr ( data, path ) :
     return sv
 #----------------------------------------------------------------------
 def blur ( data, lvl, path ) :
-    pass
+    x = data.shape[0]
+    y = data.shape[1]
+    tmp = []
+    blurr = np.copy ( data )
+
+    for i in range( 0, x ):
+        tmp.append( [] )
+        for j in range( 0, y ):
+            tmp[i].append( data[i, j] )
+    for i in range( 0, x - 1 ):
+        for j in range( 0, y - 1 ):
+            r = 0
+            g = 0
+            b = 0
+            for k in range( -1 * lvl, lvl + 1 ):
+                for h in range ( -1 * lvl, lvl + 1 ):
+                    if ( i + k < x - 1 and
+                         j + h < y - 1 and
+                         i + k >= 0 and j + h >= 0):
+                        r = r + ( tmp[i+k][j+h][0] ) * (1 / float((2*lvl+1)*(2*lvl+1)))
+                        g = g + ( tmp[i+k][j+h][1] ) * (1 / float((2*lvl+1)*(2*lvl+1)))
+                        b = b + ( tmp[i+k][j+h][2] ) * (1 / float((2*lvl+1)*(2*lvl+1)))
+                    else:
+                        break
+            blurr[i, j] = [ int(r), int(g), int(b) ]
+
+    return blurr
+#----------------------------------------------------------------------
+def blr ( data, lvl, path ) :
+    sv = path[0] + "/" + path[1].split('.')[0] + '/blur_' + path[1]
+    out = Image.fromarray( blur( data, lvl, path ), mode = 'RGB' )
+    out.save( sv )
+    return sv
+#----------------------------------------------------------------------
 def shrp ( data, path ) :
     pass
