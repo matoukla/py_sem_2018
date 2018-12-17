d2ff5295978cd7ca045fd956983463af3ff58b11
matoukla <klar.mat@seznam.cz>
2018-12-17 15:31:25 +0100

    Edited filters

--- filters.py
+++ filters.py
@@ -1,6 +1,7 @@
 from PIL import Image
 import numpy as np
 # This file contains all basic image operations and exports the result
+# Uses numpy array from imported image
 # Operations :
 #   Invert
 #   Grayscale
@@ -25,6 +26,16 @@ def gs ( data, path ) :
 
     sv = path[0] + "/" + path[1].split('.')[0] + '/gs_' + path[1]
     out = Image.fromarray( (gss + 0.5).astype( dtype = np.uint8 ),'L' )
+    out.save( sv )
+    return sv
+#----------------------------------------------------------------------
+def rt ( data, iterations, path ) :
+    rot = data
+    for i in range ( iterations ) :
+        rot = np.rot90( rot )
+
+    sv = path[0] + "/" + path[1].split('.')[0] + '/rt_'+ str( iterations ) + "_" + path[1]
+    out = Image.fromarray( rot, mode = 'RGB' )
     out.save( sv )
     return sv
 #----------------------------------------------------------------------
