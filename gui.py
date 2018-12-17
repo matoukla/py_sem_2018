699c44027ba2f3b325cd28034ab276a2f1b021d5
matoukla <klar.mat@seznam.cz>
2018-12-17 15:05:16 +0100

    Added gui.py file for Tkinter GUI of app

---
+++ gui.py
@@ -0,0 +1,10 @@
+from PIL import Image as ImagePIL
+from PIL import ImageTk
+from tkinter import *
+from tkinter.filedialog import askopenfilename
+import filters as f
+import load as l
+#----------------------------------------------------------------------
+
+class App():
+    pass
--- load.py
+++ load.py
@@ -1,6 +1,5 @@
 from PIL import Image
 import numpy as np
-
 #----------------------------------------------------------------------
 def data_image ( open_img ) :
     i = Image.open( open_img ).convert( 'RGB' )
