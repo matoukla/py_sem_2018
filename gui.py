11e270861a6f94c308970d12bd555f9f8f6267b7
matoukla <klar.mat@seznam.cz>
2018-12-17 16:39:01 +0100

    Buttons added

--- gui.py
+++ gui.py
@@ -9,16 +9,29 @@ class App( Tk ):
     def __init__( sef ):
         Tk.__init__( self )
         self.filename = ""
+        # --------------------------------------L1
+        self.l1 = Label( self, text = "basic image edits ©",
+                               foreground = "blue",
+                               font = ( "Times New Roman", 13 ) )
+        self.l1.pack()
+        # --------------------------------------B1
+        self.b1 = Button( self, text = "Browse",
+                                        width = 25,
+                                        command = self.OpenFile )
+        self.b1.pack( side = TOP )
 
         self.mainloop()
     #------------------------------------------------------------------
     def OpenFile( self ) :
         self.filename = askopenfilename (
-                 initialdir = "C:/Users/klarm/Documents/pyth",
-                 filetypes = self.formats,
-                 title = "Choose file" )
-                 
-        self.path = os.path.split( self.filename )
-        l.make_dir ( self.filename )
-        self.data_0 = l.data_image( self.filename )
+                         initialdir = "C:/Users/klarm/Documents/pyth",
+                         filetypes = self.formats,
+                         title = "Choose file" )
+        try :
+            with open( self.filename, 'r' ) as UseFile :
+                pass
+
+            self.path = os.path.split( self.filename )
+            l.make_dir ( self.filename )
+            self.data_0 = l.data_image( self.filename )
 #----------------------------------------------------------------------
