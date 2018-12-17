1f37e876af4573a385bc38ec6dc32b5b20380994
matoukla <klar.mat@seznam.cz>
2018-12-17 15:32:09 +0100

    Added function to make directory

--- load.py
+++ load.py
@@ -4,3 +4,9 @@ import numpy as np
 def data_image ( open_img ) :
     i = Image.open( open_img ).convert( 'RGB' )
     return np.asarray( i )
+#----------------------------------------------------------------------
+def make_dir ( p ) :
+    dir = p.split( '.' )
+    if not os.path.exists( dir[0] ) :
+        os.mkdir( dir[0] )
+#----------------------------------------------------------------------
