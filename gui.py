572a96df99786d8dec5f9f30b320b947a75c256f
matoukla <klar.mat@seznam.cz>
2018-12-17 16:35:50 +0100

    Open file path available now

--- gui.py
+++ gui.py
@@ -11,4 +11,14 @@ class App( Tk ):
         self.filename = ""
 
         self.mainloop()
+    #------------------------------------------------------------------
+    def OpenFile( self ) :
+        self.filename = askopenfilename (
+                 initialdir = "C:/Users/klarm/Documents/pyth",
+                 filetypes = self.formats,
+                 title = "Choose file" )
+                 
+        self.path = os.path.split( self.filename )
+        l.make_dir ( self.filename )
+        self.data_0 = l.data_image( self.filename )
 #----------------------------------------------------------------------
